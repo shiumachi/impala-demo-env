e58bc9dbc88adc0bb09311f7020edf29180a8aa2
Takahiko Sato <takabow@gmail.com>
2017-08-19 23:55:08 +0900

    Add a new cluster.conf for faster-bootstrapping

---
+++ faster-bootstrap-impala-demo-cluster/internal/internal_yum-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo yum -y install wget
+sudo yum -y install bind-utils
+sudo yum -y install git
+sudo yum -y install dnsmasq
+sudo yum -y install nmap
+sudo yum -y install xinetd
+sudo yum -y install krb5-server krb5-workstation
